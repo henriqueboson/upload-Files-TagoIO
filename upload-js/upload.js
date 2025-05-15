8d63e409c1794f84916211f9def1e61309eff057
henriqueboson <iquelrboson@gmail.com>
2025-05-15 18:07:06 -0300

    v.dev

--- upload-js/upload.js
+++ upload-js/upload.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const { Utils } = require("@tago-io/sdk");
+const { Files } = require("@tago-io/sdk");
 require("dotenv").config();
 const inquirer = require("inquirer");
 
@@ -12,34 +12,43 @@ async function main() {
     return;
   }
 
-  const { filePath } = await inquirer.prompt([
-    {
+  const answers = await inquirer.default.prompt([
+    {  
       type: "input",
       name: "filePath",
       message: "Digite o caminho do arquivo para upload:",
-      validate: (input) => fs.existsSync(input) || "Arquivo não encontrado.",
+      validate: (input) => {
+        if (!fs.existsSync(input)) return "Caminho inválido.";
+        if (fs.lstatSync(input).isDirectory()) return "Você escolheu uma pasta. Por favor, selecione um arquivo.";
+        return true;
+      }
     },
-  ]);
+  ]);  
 
+  const filePath = answers.filePath;
   const absolutePath = path.resolve(filePath);
   const buffer = fs.readFileSync(absolutePath);
   const fileName = path.basename(filePath);
+  const files = new Files({ token: TAGO_TOKEN });
 
   try {
-    const result = await Utils.files.upload(
-      {
-        filename: fileName,
-        file: buffer,
-        public: true, // ou false, se quiser privado
-      },
-      TAGO_TOKEN
-    );
+
+    const result = await files.upload({
+    filename: fileName,
+    file: buffer,
+    public: true,
+    });
 
     console.log("Upload feito com sucesso!");
     console.log("Nome:", result.filename);
     console.log("URL:", result.url);
   } catch (error) {
-    console.error("Erro ao enviar:", error.response?.data || error.message);
+    console.error("Erro ao enviar:");
+    if (error.response?.data) {
+        console.error(error.response.data);
+    } else {
+        console.error(error.message || error);
+    }
   }
 }
 
